11d6fb485531b1acba126266cc5bd2bdc5f499e1
Steven Klass <rh0dium@users.noreply.github.com>
2018-04-19 21:57:13 -0700

    Added the version and init.py

---
+++ django_zendesk/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+__name__ = 'django_zendesk'
+__author__ = 'Steven Klass'
+__version_info__ = (1, 0, 0)
+__version__ = '.'.join(map(str, __version_info__))
+__date__ = '2014/07/22 4:47:00 PM'
+__credits__ = ['Steven Klass', 'Tim Valenta']
+__license__ = 'See the file LICENSE.txt for licensing information.'
--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
+from django_zendesk import __version__
+
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
@@ -40,7 +42,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',  # Required
+    version=__version__,  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
