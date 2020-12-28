a218394381ab1939000f1321673d4b214f803d7f
artemhruzd <arti3d.artem@gmail.com>
2020-12-29 00:14:50 +0200

    Bump

--- setup.py
+++ setup.py
@@ -141,7 +141,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['django>=1.11,<4'],  # Optional
+    install_requires=['django>=1.11'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
