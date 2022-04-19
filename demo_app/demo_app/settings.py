88176761eaebc608309310be49406746513d0857
Steven K <steven@pointcircle.com>
2022-04-19 14:18:48 -0700

    set utf8 for tests.

--- demo_app/demo_app/settings.py
+++ demo_app/demo_app/settings.py
@@ -103,6 +103,7 @@ DATABASES = {
         "PASSWORD": env("MYSQL_PASSWORD"),
         "HOST": env("MYSQL_HOST"),
         "PORT": env("DOCKER_MYSQL_PORT", default=env("MYSQL_PORT", default="3306")),
+        "OPTIONS": {"read_default_file": os.path.join(Path(__file__).resolve(), "my.cnf")},
     }
 }
 
