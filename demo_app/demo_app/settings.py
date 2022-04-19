3694ecadd5b77d735e250ebad1b652c0128dc683
Steven K <steven@pointcircle.com>
2022-04-19 13:37:47 -0700

    Commit and use MySQL

--- demo_app/demo_app/settings.py
+++ demo_app/demo_app/settings.py
@@ -12,20 +12,41 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import logging
+import os
+import sys
+
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, False),
+    DEBUG_LEVEL=(int, logging.WARNING),
+    SECRET_KEY=(str, "SECRET_KEY"),
+    MYSQL_DATABASE=(str, "db"),
+    MYSQL_USER=(str, "root"),
+    MYSQL_PASSWORD=(str, "password"),
+    MYSQL_HOST=(str, "127.0.0.1"),
+    MYSQL_PORT=(str, "3306"),
+    RABBITMQ_DEFAULT_USER=(str, "guest"),
+    RABBITMQ_DEFAULT_PASS=(str, "password"),
+)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env.read_env(env_file=os.path.join(os.path.dirname(BASE_DIR), ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "%bpy6p-sy+u5pzgwwv$#%lpn5rc4nnip9!zx@f2)@rddbs$@$_"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 # Application definition
 
@@ -76,8 +97,12 @@ WSGI_APPLICATION = "demo_app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": env("MYSQL_DATABASE"),
+        "USER": env("MYSQL_USER"),
+        "PASSWORD": env("MYSQL_PASSWORD"),
+        "HOST": env("MYSQL_HOST"),
+        "PORT": env("DOCKER_MYSQL_PORT", default=env("MYSQL_PORT", default="3306")),
     }
 }
 
@@ -85,18 +110,10 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 # Internationalization
@@ -112,14 +129,52 @@ USE_L10N = True
 
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "%(asctime)s %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] - %(message)s",
+            "datefmt": "%H:%M:%S",
+        },
+    },
+    "handlers": {
+        "null": {
+            "level": "DEBUG",
+            "class": "logging.NullHandler",
+        },
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "standard",
+            "stream": sys.stdout,
+        },
+    },
+    "loggers": {
+        "django": {"handlers": ["console"], "level": "INFO", "propagate": True},
+        "django.request": {"handlers": ["console"], "level": "INFO", "propagate": False},
+        "django.security": {"handlers": ["console"], "level": "INFO", "propagate": False},
+        "django.server": {"handlers": ["console"], "level": "INFO", "propagate": False},
+        "django.db.backends": {"handlers": ["console"], "level": "INFO", "propagate": False},
+        "django.template": {"handlers": ["console"], "level": "INFO", "propagate": False},
+        "py.warnings": {"handlers": ["console"], "level": "WARNING"},
+        "demo_app": {
+            "handlers": ["console"],
+            "level": env("DEBUG_LEVEL", "ERROR"),
+            "propagate": False,
+        },
+        "": {"handlers": ["console"], "level": env("DEBUG_LEVEL", "ERROR"), "propagate": True},
+    },
+}
+
 AUTH_USER_MODEL = "users.User"
 
 ZENDESK_URL = "FOO"
 ZENDESK_TOKEN = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- demo_app/demo_app/settings_test.py
+++ demo_app/demo_app/settings_test.py
@@ -18,6 +18,17 @@ MIGRATION_MODULES = DisableMigrations()
 # Handle system warning as log messages
 warnings.simplefilter("once")
 
+for handler in LOGGING.get("handlers", []):
+    LOGGING["handlers"][handler]["level"] = "CRITICAL"
+for logger in LOGGING.get("loggers", []):
+    LOGGING["loggers"][logger]["level"] = "CRITICAL"
+
+mysql_db = DATABASES["default"]
+DEFAULT_DB = {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}
+if os.environ.get("DB_TYPE") == "mysql":
+    print("Using MySQL Backend!")
+    DEFAULT_DB = mysql_db
+
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"},
+    "default": DEFAULT_DB,
 }
