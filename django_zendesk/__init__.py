f9d6df3521105443742a268b718248b75d16afc2
Steven K <steven@pointcircle.com>
2023-01-30 15:41:12 +0000

    Version Bump [skip ci]

--- django_zendesk/__init__.py
+++ django_zendesk/__init__.py
@@ -2,8 +2,8 @@
 
 __name__ = "django_zendesk"
 __author__ = "Pivotal Energy Solutions"
-__version_info__ = (2, 1, 15)
-__version__ = "2.1.15"
+__version_info__ = (2, 1, 16)
+__version__ = "2.1.16"
 __date__ = "2014/07/22 4:47:00 PM"
 __credits__ = ["Steven Klass", "Tim Valenta"]
 __license__ = "See the file LICENSE.txt for licensing information."
